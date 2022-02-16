abb52f013ae590ab3a1ae9e7dd45c20db761b63a
alvaroeduardo <alvaroedusilva@yahoo.com>
2022-02-15 23:52:15 -0300

    Puxando dados do shoutcast.

--- src/Controller/radioData.controller.js
+++ src/Controller/radioData.controller.js
@@ -1,14 +1,15 @@
 const puppeteer = require('puppeteer');
 
 module.exports = {
-    async radio(req, res){
+
+    async data(req, res){
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.goto('https://example.com');
-        await page.screenshot({ path: 'example.png' });
+        await page.goto('http://stream2.svrdedicado.org:8160/');
+        const data = await page.evaluate(() => Array.from(document.querySelectorAll('b'), e => e.innerText));
 
         await browser.close();
 
-        return res.json({ ok: 'ok' })
+        return res.json({Ouvintes: data[5], Locutor: data[7], Programa: data[8], Música: data[10]})
     }
 }
--- src/router.js
+++ src/router.js
@@ -4,6 +4,6 @@ const routes = express.Router();
 
 const radioData = require('./Controller/radioData.controller')
 
-routes.get('/home', radioData.radio)
+routes.get('/home', radioData.data)
 
 module.exports = routes;
